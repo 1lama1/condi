da47e59b52ebe4e59aafec443a06e82d413a3e9d
user <fatkat100@mail.ru>
2022-07-14 22:57:05 +0300

    второй коммит

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Start {
     public static void main(String[] args) {
+        String AndIso;
+        System.out.println("Введите платформу (Android/iSO) :");
+        Scanner rty = new Scanner(System.in);
+        AndIso = rty.next();
+        if (AndIso.equals("Android")) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (AndIso.equals("iSO")) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Ошибка");
 
+        }
     }
 }
